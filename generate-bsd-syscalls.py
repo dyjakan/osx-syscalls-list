11d150925b5b29b94124f7b607f44930a870779b
Andrzej Dyjak <andrzej@datanauts.co.uk>
2016-10-20 10:36:32 +0100

    Update for silent  xnu-3789.1.32

--- generate-bsd-syscalls.py
+++ generate-bsd-syscalls.py
@@ -322,7 +322,7 @@ if __name__ == "__main__":
 		sys.exit(1)
 
 	PATH_XNU_SOURCE = sys.argv[1]
-	URL_XNU_SOURCE = "http://opensource.apple.com/source/xnu/xnu-3248.60.10/"
+	URL_XNU_SOURCE = "https://opensource.apple.com/source/xnu/xnu-3789.1.32/"
 	PATH_EXUBERANT_CTAGS = "/usr/local/Cellar/ctags/5.8_1/bin/ctags"
 	PATH_BSD_SYSCALLS = PATH_XNU_SOURCE + "bsd/kern/syscalls.master"
 	PATH_BSD_TEMP_FILE = "/tmp/bsd-syscall-tmp"
