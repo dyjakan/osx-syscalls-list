d5720b43e91b67dced4f23e373bb0c4a9afd4e6c
Andrzej Dyjak <dyjakan@sigsegv.pl>
2017-06-24 15:35:16 +0200

    Fix variable name

--- generate-bsd-syscalls.py
+++ generate-bsd-syscalls.py
@@ -341,8 +341,8 @@ if __name__ == "__main__":
 	BANNER = "<h1>macOS BSD System Calls</h1>\n"
 	v1 = PATH_XNU_SOURCE[PATH_XNU_SOURCE.find("xnu-"):]
 	ts = datetime.datetime.now().strftime("%A, %d %B %Y")
-	github = "<a href=\"https://twitter.com/andrzejdyjak\">@andrzejdyjak</a>"
-	BANNER += "<p>Generated from <i><a href=\"" + URL_XNU_SOURCE + "\">" + v1.upper() + "</a></i> on <i>" + ts + "</i> by " + github + ".</p>\n"
+	twitter = "<a href=\"https://twitter.com/andrzejdyjak\">@andrzejdyjak</a>"
+	BANNER += "<p>Generated from <i><a href=\"" + URL_XNU_SOURCE + "\">" + v1.upper() + "</a></i> on <i>" + ts + "</i> by " + twitter + ".</p>\n"
 	BANNER += "<p>Description for <a href=\"osx-bsd-syscalls.json\">JSON dump</a> elements (apart from <i>conditionals</i>):</p>"
 	BANNER += "<pre>[\n\tsyscall number,\n\treturn type,\n\tsyscall name,\n\tnumber of args,\n\targ 1, ..., arg n,\n\tsource\n]</pre>"
 	BANNER += "<p>You can find github repository <a href=\"https://github.com/dyjakan/osx-syscalls-list\">here</a>. Feedback, ideas, bugs, <i>et cetera</i> &#8211; <a href=\"https://sigsegv.pl\">give me a shout</a>.</p>\n"
