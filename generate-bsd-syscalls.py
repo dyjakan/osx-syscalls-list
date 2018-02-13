bde5378fedcc7a80aca7dc7f4aec6b56fc090c65
Andrzej Dyjak <dyjakan@sigsegv.pl>
2018-02-13 13:21:53 +0100

    Update for xnu-4570.41.2

--- generate-bsd-syscalls.py
+++ generate-bsd-syscalls.py
@@ -330,7 +330,7 @@ if __name__ == "__main__":
 		sys.exit(1)
 
 	PATH_XNU_SOURCE = sys.argv[1]
-	URL_XNU_SOURCE = "https://opensource.apple.com/source/xnu/xnu-4570.20.62/"
+	URL_XNU_SOURCE = "https://opensource.apple.com/source/xnu/xnu-4570.41.2/"
 	PATH_EXUBERANT_CTAGS = "/usr/local/Cellar/ctags/5.8_1/bin/ctags"
 	PATH_BSD_SYSCALLS = PATH_XNU_SOURCE + "bsd/kern/syscalls.master"
 	PATH_BSD_TEMP_FILE = "/tmp/bsd-syscall-tmp"
