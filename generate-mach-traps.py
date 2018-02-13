4ce4b011fc7afe713a0d0df30bec45bf834ffd96
Andrzej Dyjak <dyjakan@sigsegv.pl>
2018-02-13 13:05:30 +0100

    Update for xnu-4570.20.62

--- generate-mach-traps.py
+++ generate-mach-traps.py
@@ -321,7 +321,7 @@ if __name__ == "__main__":
 		sys.exit(1)
 
 	PATH_XNU_SOURCE = sys.argv[1]
-	URL_XNU_SOURCE = "https://opensource.apple.com/source/xnu/xnu-4570.1.46/"
+	URL_XNU_SOURCE = "https://opensource.apple.com/source/xnu/xnu-4570.20.62/"
 	PATH_EXUBERANT_CTAGS = "/usr/local/Cellar/ctags/5.8_1/bin/ctags"
 	PATH_MACH_TRAPS = PATH_XNU_SOURCE + "osfmk/kern/syscall_sw.c"
 	PATH_MACH_TRAPS_ARGS = PATH_XNU_SOURCE + "osfmk/mach/mach_traps.h"
